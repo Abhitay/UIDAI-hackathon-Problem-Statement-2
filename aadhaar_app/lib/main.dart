fc5c2f86282ce2a5fd7bf12f4ea47a63c1276fc6
Abhitay <59016268+Abhitay@users.noreply.github.com>
2021-10-27 17:14:31 +0530

    Update main.dart

--- aadhaar_app/lib/main.dart
+++ aadhaar_app/lib/main.dart
@@ -5,9 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('GeeksforGeeks'),
+    ),
+    body: Column(children: [
+      Container(child:)
+    ]),
+    );
   }
 }
